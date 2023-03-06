821788318526b939efcd2279191152045d6bcf1e
zied-khili <ziedkhii7@gmail.com>
2023-03-06 15:24:21 +0100

    kk

--- main.js
+++
@@ -1,4 +0,0 @@
-// animating: fading in text
-$(document).ready(function(){
-    $("#welcome").fadeIn(1000,linear)
-})
---
+++ products.js
@@ -0,0 +1,188 @@
+/*                  GOLD                  */
+// bracelets of gold 
+var bracelets1={
+    src:"Pictures/Gold/bracelets/bracelets1.jpg",
+    price:800
+}
+ var bracelets2={
+    src:"Pictures/Gold/bracelets/bracelets2.jpg",
+    price:500
+ }
+ var bracelets3 ={
+    src:"Pictures/Gold/bracelets/bracelets3.jpg",
+    price:1000
+ }
+ var bracelets4={
+    src:"Pictures/Gold/bracelets/bracelets4.jpg",
+    price:700
+ }
+ var bracelets5={
+    src:"Pictures/Gold/bracelets/bracelets5.jpg",
+    price:4500
+ }
+ var bracelets6={
+    src:"Pictures/Gold/v/bracelets6.jpg",
+    price:3000
+ }
+ // earing of Gold
+
+ var earrings={
+    src:"Pictures/Gold/earrings/1.jpg",
+    price:500
+ }
+ // necklace of Gold
+var necklace1={
+    src:"Pictures/Gold/necklace/2.jpg",
+    price:1800
+}
+var necklace2={
+    src:"Pictures/Gold/necklace/3.jpg",
+    price:2000
+}
+ //rings of Gold
+var rings1={
+    src:"Pictures/Gold/rings/1.jpg",
+    price:600
+}
+var rings2={
+    src:"Pictures/Gold/rings/2.jpg",
+    price:500
+}
+ var rings3={
+    src:"Pictures/Gold/rings/3.jpg",
+    price:600
+ }
+ var rings4={
+    src:"Pictures/Gold/rings/4.jpg",
+    price:500
+ }
+ var rings5={
+    src:"Pictures/Gold/rings/5.jpg",
+    price:400
+ }
+ /*                                 Silver                                    */
+ // bracelets of Silver
+ var brace1={
+    src:"Pictures/Silver/braceSilver/1.jpg",
+    price:50
+ }
+ var brace2={
+    src:"Pictures/Silver/braceSilver/2.jpg",
+    price:50
+ }
+ var brace3={
+    src:"Pictures/Silver/braceSilver/3.jpg",
+    price:80
+ }
+ var brace4={
+    src:"Pictures/Silver/braceSilver/4.jpg",
+    price:40
+ }
+ var brace4={
+    src:"Pictures/Silver/braceSilver/4.jpg",
+    price:40
+ }
+ var brace5={
+    src:"Pictures/Silver/braceSilver/5.jpg",
+    price:40
+ }
+ var brace6={
+    src:"Pictures/Silver/braceSilver/.jpg",
+    price:40
+ }
+
+ var brace7={
+    src:"Pictures/Silver/braceSilver/7.jpg",
+    price:80
+ }
+ var brace8={
+    src:"Pictures/Silver/braceSilver/8.jpg",
+    price:80
+ }
+
+ var brace9={
+    src:"Pictures/Silver/braceSilver/9.jpg",
+    price:120
+ }
+ // necklaces of Silver
+ var necklace1={
+    src:"Pictures/Silver/necklaces/1.jpg",
+    price:80
+ }
+ var necklace2={
+    src:"Pictures/Silver/necklaces/2.jpg",
+    price:80
+ }
+ var necklace3={
+    src:"Pictures/Silver/necklaces/3.jpg",
+    price:80
+ }
+ var necklace4={
+    src:"Pictures/Silver/necklaces/4.jpg",
+    price:50
+ }
+ var necklace5={
+    src:"Pictures/Silver/necklaces/5.jpg",
+    price:110
+ }
+ var necklace6={
+    src:"Pictures/Silver/necklaces/6.jpg",
+    price:70
+ }
+ var necklace7={
+    src:"Pictures/Silver/necklaces/7.jpg",
+    price:70
+ }
+ var necklace8={
+    src:"Pictures/Silver/necklaces/8.jpg",
+    price:80
+ }
+ var necklace9={
+    src:"Pictures/Silver/necklaces/9.jpg",
+    price:40
+ }
+  // rings of Silver 
+  var ring1={
+    src:"Pictures/Silver/ringsSilver/1.jpg",
+    price:45
+  }
+  var ring2={
+    src:"Pictures/Silver/ringsSilver/2.jpg",
+    price:45
+  }
+  var ring3={
+    src:"Pictures/Silver/ringsSilver/3.jpg",
+    price:45
+  }
+  //traditional of Silver 
+  var traditional1={
+    src:"Pictures/Silver/traditional/1.jpg",
+    price:700
+  }
+  var traditional2={
+    src:"Pictures/Silver/traditional/2.jpg",
+    price:200
+  }
+  var traditional3={
+    src:"Pictures/Silver/traditional/3.jpg",
+    price:300
+  }
+  var traditional4={
+    src:"Pictures/Silver/traditional/4.jpg",
+    price:700
+  }
+  // braceletstraditional of Silver
+  var tradi1={
+    src:"Pictures/Silver/tradi/1.jpg",
+    price:70
+  }
+  var tradi2={
+    src:"Pictures/Silver/tradi/2.jpg",
+    price:70
+  }
+
+
+
+
+
+
