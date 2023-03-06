25f4e66407a631c41c2cfd674b99d92f694d31d1
RayenTellissy <rayentellissy571@gmail.com>
2023-03-06 15:30:10 +0100

    .

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 // animating: fading in text
-$(document).ready(function(){
-    $("#welcome").fadeIn(1000,linear)
-})
+$("#wdiv").fadeOut(1)
+$("#main").fadeOut(1)
+$("#wdiv").fadeIn(2500)
+
+function leaveWelcome(){  //fadeout on click
+    $("#wdiv").fadeOut(1000,function(){
+        $("#main").fadeIn(1000)
+    })
+}
+
+function gold(){
+    $("#main").fadeOut(1000)
+}
