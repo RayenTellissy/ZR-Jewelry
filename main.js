d98ef5f2b6b494e77725a01216c7181b9e6a2ad2
zied-khili <ziedkhii7@gmail.com>
2023-03-06 18:00:08 +0100

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // animating: fading in text
 $("#wdiv").fadeOut(1)
 $("#main").fadeOut(1)
+$("#gold").fadeOut(1)
 $("#wdiv").fadeIn(2500)
 
 function leaveWelcome(){  //fadeout on click
@@ -11,4 +12,9 @@ function leaveWelcome(){  //fadeout on click
 
 function gold(){
     $("#main").fadeOut(1000)
+    $("#gold").fadeIn(1000)
 }
+// 
+var brace1=$('<div class="bracelets"><img src='+bracelets1.src+' alt="tradii"width="230",height="220"><p>'+bracelets1.name+'</p><p>Price:'+bracelets1.price+'</p></div>')
+$("#goldbracelets").append(brace1)
+
